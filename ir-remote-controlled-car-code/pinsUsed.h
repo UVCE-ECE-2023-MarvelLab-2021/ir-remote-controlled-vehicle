a63a21619f5371f61a46ba6644bfbfbf35807060
Yuan Biju <yuankavumkal652@gmail.com>
2022-04-20 21:03:56 +0530

    first commit;contains code

---
+++ ir-remote-controlled-car-code/pinsUsed.h
@@ -0,0 +1,11 @@
+#pragma once
+
+int ir_recv_pin = 4;
+
+// Bot wheel direction control pins
+int rf_pin = 5; // right wheels forward
+int rr_pin = 6; //right wheels reverse
+int lf_pin = 9; // left wheels forward
+int lr_pin = 10;  // left wheels reverse
+
+int botSpeed = 75; // speed of wheels
